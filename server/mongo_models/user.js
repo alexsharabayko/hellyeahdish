ce57463baf7e32e0c9577908d8cd12724ed1b7b6
alexsharabayko <alexsharabayko@gmail.com>
2015-08-20 18:29:28 +0300

    Added user model to server logic

---
+++ server/mongo_models/user.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    firstName: String,
+    lastName: String,
+    level: {
+        type: Number,
+        default: 0
+    },
+    token: {
+        type: String,
+        default: ''
+    },
+    createdAt: Date
+});
+
+UserSchema.pre('save', function (next) {
+    this.createdAt = Date.now();
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express'),
     app = express(),
-    port = 3111,
+    port = 4000,
     mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    User = require('./mongo_models/user');
 
-mongoose.connect('localhost:27017/moneyTrashServer');
+mongoose.connect('localhost:27017/hellyeahdish');
 
 app.use(allowCrossDomain);
 
@@ -12,9 +13,52 @@ app.use(bodyParser());
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+router.route('/users')
+    .get(function (req, res) {
+        User.find(function (err, users) {
+            err && res.send(err);
+
+            res.json(users);
+        })
+    })
+    .post(function (req, res) {
+        var username = req.body.username,
+            password = req.body.password,
+            email = req.body.email;
+
+        if (!(username && password && email)) {
+            res.code(400).json({ message: 'Bad data' });
+        }
+
+        var user = new User({
+            username: username,
+            password: password,
+            email: email,
+            firstName: req.body.firstName || '',
+            lastName: req.body.lastName || ''
+        });
+
+        user.save(function (err) {
+            if (err) {
+                res.code(500).send({ message: 'Internal error with save user' });
+            }
+
+            res.json({ message: 'Ok', id: user._id });
+        });
+    });
+
+router.route('/users/:id')
+    .get(function (req, res) {
+        res.json({ message: 'Get user ' + req.params.id });
+    })
+    .put(function (req, res) {
+        res.json({ message: 'Put user ' + req.params.id });
+    })
+    .delete(function (req, res) {
+        res.json({ message: 'Delete user ' + req.params.id });
+    });
+
+app.use('/', router);
 
 app.listen(port);
 console.log('Listening port ' + port);
