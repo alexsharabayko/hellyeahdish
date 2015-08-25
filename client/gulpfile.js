4f5387fcd772523449486d0b7d782f44f826d81c
alexsharabayko <alexsharabayko@gmail.com>
2015-08-25 18:38:43 +0300

    Added webpack to the client

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    webpack = require('gulp-webpack');
 
 gulp.task('sass', function () {
     gulp.src('sass/**/*.scss')
@@ -7,6 +8,18 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', ['sass'], function () {
-    gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('webpack', function () {
+    return gulp.src('js/app.js')
+        .pipe(webpack({
+            watch: true,
+            output: {
+                path: __dirname,
+                filename: 'bundle.js'
+            }
+        }))
+        .pipe(gulp.dest('js/'));
 });
+
+//gulp.task('watch', ['sass'], function () {
+//    gulp.watch('sass/**/*.scss', ['sass']);
+//});
